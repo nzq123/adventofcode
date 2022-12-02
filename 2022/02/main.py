b8487bd315275db518ac12f539b2c865e5aa835f
nzq123 <enzet10@gmail.com>
2022-12-02 20:04:11 +0100

    second excercise second step

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -4,28 +4,6 @@ with open("elf2exc.txt", 'r') as f:
 computer = 0
 player = 0
 
-for i in elfs:
-    if i[0] == 'A' and i[2] == 'Y':
-        player += 8
-    if i[0] == 'A' and i[2] == 'X':
-        player += 4
-    if i[0] == 'A' and i[2] == 'Z':
-        player += 3
-    if i[0] == 'B' and i[2] == 'Y':
-        player += 5
-    if i[0] == 'B' and i[2] == 'X':
-        player += 1
-    if i[0] == 'B' and i[2] == 'Z':
-        player += 9
-    if i[0] == 'C' and i[2] == 'Y':
-        player += 2
-    if i[0] == 'C' and i[2] == 'X':
-        player += 7
-    if i[0] == 'C' and i[2] == 'Z':
-        player += 6
-
-print(player)
-
 # KOMPUTER
 # A = ROCK
 # B = PAPER
@@ -36,6 +14,34 @@ print(player)
 # Z = SCIZOR 3
 #
 # WIN = 6, DRAW = 3, LOSE = 0
+#
+# Y = DRAW
+# X = LOSE
+# Z = WIN
+
+
+for i in elfs:
+    if i[0] == 'A' and i[2] == 'Y':
+        player += 4
+    if i[0] == 'A' and i[2] == 'X':
+        player += 3
+    if i[0] == 'A' and i[2] == 'Z':
+        player += 8
+    if i[0] == 'B' and i[2] == 'Y':
+        player += 5
+    if i[0] == 'B' and i[2] == 'X':
+        player += 1
+    if i[0] == 'B' and i[2] == 'Z':
+        player += 9
+    if i[0] == 'C' and i[2] == 'Y':
+        player += 6
+    if i[0] == 'C' and i[2] == 'X':
+        player += 2
+    if i[0] == 'C' and i[2] == 'Z':
+        player += 7
+
+print(player)
+
 
 
 
